105c5e12a40c525303419d6c25b0f4ed654ab106
Nayem Uddin <nayemuddincucse@gmail.com>
2024-09-27 15:28:07 +0600

    domino problem solved

--- Solution.java
+++ Solution.java
@@ -112,3 +112,17 @@ class BitLandBit {
         input.close();
     }
 }
+
+class Domino {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        int m = input.nextInt();
+        int n = input.nextInt();
+
+        System.out.println(m * n / 2);
+
+        input.close();
+
+    }
+}
